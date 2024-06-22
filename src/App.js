1a1ba687e2181884de573b04d3fadd9e6a26df14
Osiyomeoh <samuelaleonomoh5@gmail.com>
2024-06-22 12:40:33 +0100

    chore: add new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ContactForm from './components/contact.form';
+import ContactList from './components/contact.list';
 
-function App() {
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [contactToEdit, setContactToEdit] = useState(null);
+
+  const addContact = (contact) => {
+    setContacts([...contacts, contact]);
+  };
+
+  const editContact = (updatedContact) => {
+    setContacts(
+      contacts.map((contact) => (contact.email === updatedContact.email ? updatedContact : contact))
+    );
+    setContactToEdit(null);
+  };
+
+  const deleteContact = (contactToDelete) => {
+    setContacts(contacts.filter((contact) => contact.email !== contactToDelete.email));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Contact Management System</h1>
+      <ContactForm
+        addContact={addContact}
+        editContact={editContact}
+        contactToEdit={contactToEdit}
+        setContactToEdit={setContactToEdit}
+      />
+      <ContactList contacts={contacts} deleteContact={deleteContact} editContact={setContactToEdit} />
     </div>
   );
-}
+};
 
 export default App;
